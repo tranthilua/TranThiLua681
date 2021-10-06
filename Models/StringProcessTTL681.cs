bd87fd993045e1c2d4626426a396b0f21e3b85b9
LUA TRAN THI <1821050681@student.humg.edu.vn>
2021-10-06 09:12:59 +0700

    Create_Table_TTL681

--- Models/StringProcessTTL681.cs
+++ Models/StringProcessTTL681.cs
@@ -7,16 +7,16 @@ using System.Collections.Generic;
 namespace TTL681.Models
 
 {
-    public class HTT0774
+    public class TTL0681
     {
         [Key]
-        public string HTTId { get; set; }
+        public string TTLId { get; set; }
          [Display(Name = "Tên")]
         public string HTTName { get; set; }
          [Display(Name = "Giới Tính")]
         public string TTLGender { get; set; }
     }
-    public class StringProcessHTT774 : HTT0774
+       public class StringProcessTTL681 : TTL0681
     {
        
         public string TTLName { get; set; }
