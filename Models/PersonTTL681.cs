1ba50dce303660e929216052685fb8f97e5ee8ca
LUA TRAN THI <1821050681@student.humg.edu.vn>
2021-10-06 08:13:25 +0700

    donedone

--- Models/PersonTTL681.cs
+++ Models/PersonTTL681.cs
@@ -3,13 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TranThiLua681.Models
 {
-    public class TranThiLua681
+    public class PersonTTL681
 
     {   [Key]
         [StringLength(20, MinimumLength = 3)]
         public string PersonId { get; set; }
-        [StringLength(50, MinimumLength = 3)]
-        [Required]
+         [StringLength(50, MinimumLength = 3)]
+         [Required]
         public string PersonName { get; set; }
 
         
--- Startup.cs
+++ Startup.cs
@@ -8,22 +8,39 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace TranThiLua681
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        Environment = env;
+        Configuration = configuration;
+    }
 
         public IConfiguration Configuration { get; }
+         public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+           services.AddDbContext<AplicationContext>(options =>
+        {
+            var connectionString = Configuration.GetConnectionString("AplicationContext");
+
+            if (Environment.IsDevelopment())
+            {
+                options.UseSqlite(connectionString);
+            }
+            else
+            {
+                options.UseSqlServer(connectionString);
+            }
+        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
